6eff623cbee56a56a762e2e8707f644b75d21a8f
Johannes Fichte <daajoe@ficht.es>
2019-12-12 18:57:15 +0100

    SAT transparent cat

--- tools/sat/bin/sat_solver.sh
+++ tools/sat/bin/sat_solver.sh
@@ -31,6 +31,7 @@ if [ -z $solver ] ; then
   echo "No Solver given. Exiting..."
   exit 1
 fi
+solver_cmd="./$solver"_glibc $@
 
 if [ ! -z $thp ] ; then
   echo "No filename given. Exiting..."
@@ -41,12 +42,25 @@ if [ -n $thp ] ; then
   export GLIBC_THP_ALWAYS=1
   echo "Using THP option in libc"
 fi
-cmd="./"$solver"_glibc $@ $filename"
-echo $cmd
+
+type=$(file -b --mime-type $filename)
+echo $type
+
+if [ $type == "application/x-lzma" ] ; then
+  cmd="lzcat $filename"
+elif [ $type == "application/x-bzip2" ] ; then
+  cmd="bzcat $filename"
+elif [ $type == "application/x-xz" ] ; then
+  cmd="xzcat $filename"
+else
+  cmd="zcat -f $filename"
+fi
+
 
 cd "$(dirname "$0")"
-bash -c 'echo GLIBC_THP_ALWAYS=$GLIBC_THP_ALWAYS'
-bash -c "$cmd" &
+echo "$cmd | $solver_cmd"
+echo GLIBC_THP_ALWAYS=$GLIBC_THP_ALWAYS
+$cmd | $solver_cmd &
 PID=$!
 wait $PID
 exit $?
