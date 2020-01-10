9566e4768bbfa558fd0a837be85270bf2780bb4b
Johannes Fichte <daajoe@ficht.es>
2020-01-10 12:12:13 +0100

    Added MaxSAT

---
+++ tools/maxsat/bin/maxsat_solver.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+verbose=0
+thp=0
+while getopts "h?vt:s:f:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    v)  verbose=1
+        ;;
+    t)  thp=1
+        ;;
+    s)  solver=$OPTARG
+        ;;
+    f)  filename=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+trap 'kill -TERM $PID' TERM
+
+if [ -z $solver ] ; then
+  echo "No Solver given. Exiting..."
+  exit 1
+fi
+solver_cmd="./$solver"_glibc $@
+
+if [ -z $filename ] ; then
+  echo "No filename given. Exiting..."
+  exit 1
+fi
+
+if [ ! -f $filename ] ; then
+  echo "Filename does not exist. Exiting..."
+  exit 1
+fi
+
+if [ $thp == 1 ] ; then
+  env="GLIBC_THP_ALWAYS=1"
+fi
+
+echo $env
+
+type=$(file -b --mime-type $filename)
+echo $type
+
+if [ $type == "application/x-lzma" ] ; then
+  cmd="lzcat $filename"
+elif [ $type == "application/x-bzip2" ] ; then
+  cmd="bzcat $filename"
+elif [ $type == "application/x-xz" ] ; then
+  cmd="xzcat $filename"
+else
+  cmd="zcat -f $filename"
+fi
+
+
+cd "$(dirname "$0")"
+echo "$cmd | env $env $solver_cmd"
+$cmd | $solver_cmd &
+PID=$!
+wait $PID
+exit $?
+
