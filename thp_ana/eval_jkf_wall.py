05ac15523e06fe6100050bf27887e6e04d91fa2e
Johannes Fichte <daajoe@ficht.es>
2020-01-14 17:46:53 +0100

    Parallel SAT Executors

---
+++ thp_ana/eval_jkf_wall.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+df = pd.read_csv('output_asp_solvers_2020_01_14.csv')
+# print(df)
+
+
+df['group'] = df['run_id'].apply(lambda row: row.split('/')[1])
+df['solver'] = df['run_id'].apply(lambda row: row.split('/')[2])
+df['instance'] = df['run_id'].apply(lambda row: '/'.join(row.split('/')[3:]))
+
+solver = df[df.solver=='default[s=minisat]']
+print('*'*80)
+print('--- NONTHP ---')
+glibc=solver[solver.group=='glibc']
+# print(glibc)
+print('*'*80)
+print('--- THP ---')
+glibc_thp=solver[solver.group=='glibc_thp']
+
+merged = pd.merge(glibc, glibc_thp, on='instance', how='outer')
+# print(merged)
+
+filtered = merged[~(((merged.verdict_x == 'OK') & (merged.verdict_y == 'OK'))) & ~((merged.verdict_x == 'TLE') & (merged.verdict_y == 'TLE'))]
+# printme = ['solver_x', 'instance','runsolver_WCTIME_x', 'perf_tlb_miss_x', 'return_code_x', 'verdict_x', 'runsolver_WCTIME_y', 'runsolver_STATUS_y', 'perf_tlb_miss_y', 'verdict_y']
+printme = ['solver_x', 'instance','runsolver_WCTIME_x', 'return_code_x', 'verdict_x', 'runsolver_WCTIME_y', 'runsolver_STATUS_y', 'verdict_y']
+filtered = filtered[printme].sort_values(by=['instance'])
+# print(filtered)
+
+df_red=df[['instance','wall_time','perf_elapsed','solver','verdict', 'group']]
+df1=df_red
+
+# df1=df_red[df_red.solver=='default[s=plingeling]']
+# df1=df_red[df_red.solver=='default[s=minisat]']
+# df1=df_red[df_red.solver=='default[s=mergesat]']
+# df1=df1[df1.verdict=='OK']
+# print(df1)
+
+# x=df1.groupby('group').agg({'wall_time': np.median})
+x=df1.groupby(['group','solver','verdict']).agg('wall_time')
+print(x.describe())
+# print(x)
---
+++ tools/psat/bin/psat_solver.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+verbose=0
+thp=0
+while getopts "h?vt:s:f:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    v)  verbose=1
+        ;;
+    t)  thp=1
+        ;;
+    s)  solver=$OPTARG
+        ;;
+    f)  filename=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+function interrupted(){
+  kill -TERM $PID
+}
+trap interrupted TERM
+trap interrupted INT
+
+
+if [ -z $solver ] ; then
+  echo "No Solver given. Exiting..."
+  exit 1
+fi
+
+if [ -z $filename ] ; then
+  echo "No filename given. Exiting..."
+  exit 1
+fi
+
+if [ ! -f $filename ] ; then
+  echo "Filename does not exist. Exiting..."
+  exit 1
+fi
+
+if [ $thp == 1 ] ; then
+  env="GLIBC_THP_ALWAYS=1"
+fi
+
+cd "$(dirname "$0")"
+
+#get basic info
+source ../../bash_shared/sysinfo.sh
+
+#get file transparently from compressed file and temporarily store in shm
+source ../../bash_shared/tcat.sh
+
+if [ "$solver" == "plingeling" ] ; then
+  solver_cmd="./"$solver"_glibc -t 1 -g 8 $@"
+#elif [ "$solver" == "mergesat" ] ; then
+else
+  solver_cmd="./$solver"_glibc $@
+fi
+
+echo "cat $decomp_filename | env $env $solver_cmd"
+#run call in background and wait for finishing
+cat $decomp_filename | $solver_cmd &
+PID=$!
+wait $PID
+exit $?
