90561d7d1dafe681e4aabac952d166315628f003
Johannes Fichte <daajoe@ficht.es>
2020-02-08 12:27:17 +0100

    Additional Solver

--- experiment/thp/tool/psat/bin/psat_solver.sh
+++ experiment/thp/tool/psat/bin/psat_solver.sh
@@ -4,21 +4,24 @@
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 # Initialize our own variables:
-verbose=0
+#verbose=0
 thp=0
-while getopts "h?vt:s:f:" opt; do
+while getopts "h?vt:s:f:i:" opt; do
     case "$opt" in
     h|\?)
         show_help
         exit 0
         ;;
-    v)  verbose=1
+    v)  echo "Currently unsupported."
+        #verbose=1
         ;;
-    t)  thp=1
+    t)  thp=$OPTARG
         ;;
     s)  solver=$OPTARG
         ;;
     f)  filename=$OPTARG
+        ;;
+    i)  original_input=$OPTARG
         ;;
     esac
 done
@@ -48,13 +51,39 @@ if [ ! -f $filename ] ; then
 fi
 
 if [ $thp == 1 ] ; then
-  env="GLIBC_THP_ALWAYS=1"
+  env=GLIBC_THP_ALWAYS=1
+else
+  env=VOID=1
 fi
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || (echo "Could not change directory to $0. Exiting..."; exit 1)
 
-#get basic info
-source ../../bash_shared/sysinfo.sh
+
+if [ "$solver" == "plingeling" ] ; then
+  solver_cmd="./"$solver"_glibc -t 1 -g 8 $*"
+#elif [ "$solver" == "mergesat" ] ; then
+else
+  solver_cmd="./"$solver"_glibc $*"
+fi
+
+echo "Original input instance was $original_input"
+echo "env $env $solver_cmd $filename"
+echo
+echo
+
+
+#NOTE: if you need to redirect the solver output in the future, we suggest to use stdlog.txt
+#
+# run call in background and wait for finishing
+env $env $solver_cmd $filename &
+#alternative approach
+#(export $env; $solver_cmd $filename) &
+PID=$!
+wait $PID
+echo "Solver finished with exit code="$?
+exit $?
+
+
 
 if [ "$solver" == "plingeling" ] ; then
   solver_cmd="./"$solver"_glibc -t 4 -g 8 $@"
